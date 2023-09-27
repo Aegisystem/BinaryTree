830fbf329561ddf7d99f6430c216f2c222db6f92
Steven Correa <97488936+Aegisystem@users.noreply.github.com>
2023-09-26 21:23:36 -0500

    Tree initialized

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ class Tree {
         this.root = null
     }
 
-    insertNode(value) {
-        let newNode = new Node(value)
+    insertNode(name, value) {
+        let newNode = new Node(name, value)
         if (this.root === null) {
             this.root = newNode
             return
@@ -35,24 +35,39 @@ class Tree {
         if(node === null) {
             return
         }
-        order === 1 ? console.log(node.value) : null
+        order === 1 ? console.log(node.name) : null
         this.printOrder(node.left, order)
-        order === 2 ? console.log(node.value) : null
+        order === 2 ? console.log(node.name) : null
         this.printOrder(node.right, order)
-        order === 3 ? console.log(node.value) : null
+        order === 3 ? console.log(node.name) : null
     }
 }
 
 class Node {
-    constructor(value) {
+    constructor(value, name) {
         this.value = value
+        this.name = name
         this.left = null
         this.right = null
     }
 }
 
-const tree = new Tree()
-tree.insertNode(10)
-tree.insertNode(3)
-tree.insertNode(11)
-tree.printOrder(tree.root, 3)
+const dataPoint2 = () => {
+    const tree = new Tree()
+    tree.insertNode(50, 'A')
+    tree.insertNode(25, 'B')
+    tree.insertNode(75, 'C')
+    tree.insertNode(20, 'D')
+    tree.insertNode(37, 'E')
+    tree.insertNode(90, 'F')
+    tree.insertNode(10, 'G')
+    tree.insertNode(30, 'H')
+    tree.insertNode(39, 'I')
+    tree.insertNode(82, 'J')
+    tree.insertNode(26, 'K')
+    tree.insertNode(86, 'L')
+    return tree;
+}
+
+const tree2 = dataPoint2()
+tree2.printOrder(tree2.root, 3)
