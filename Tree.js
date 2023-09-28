955d03dbb15a9f848716ce7960e6014852b4f917
Steven Correa <97488936+Aegisystem@users.noreply.github.com>
2023-09-28 14:14:52 -0500

    Logic complete

--- Tree.js
+++ Tree.js
@@ -1,3 +1,12 @@
+class Node {
+    constructor(value, name) {
+        this.value = value
+        this.name = name
+        this.left = null
+        this.right = null
+    }
+}
+
 class Tree {
     constructor() {
         this.root = null
@@ -28,6 +37,26 @@ class Tree {
         }
     }
     
+    searchNode(value) {
+        if (this.root === null) {
+            return null
+        }
+        return this.searchNodeHelper(this.root, value)
+    }
+
+    searchNodeHelper(node, value) {
+        if (node === null) {
+            return null
+        }
+        if (node.value === value) {
+            return node
+        } else if (value < node.value) {
+            return this.searchNodeHelper(node.left, value)
+        } else {
+            return this.searchNodeHelper(node.right, value)
+        }
+    }
+
     // 1 - Preorder
     // 2 - Inorder
     // 3 - Postorder
@@ -41,16 +70,9 @@ class Tree {
         this.printOrder(node.right, order)
         order === 3 ? console.log(node.name) : null
     }
+
 }
 
-class Node {
-    constructor(value, name) {
-        this.value = value
-        this.name = name
-        this.left = null
-        this.right = null
-    }
-}
 
 const dataPoint2 = () => {
     const tree = new Tree()
@@ -69,5 +91,22 @@ const dataPoint2 = () => {
     return tree;
 }
 
-const tree2 = dataPoint2()
+const dataPoint3 = () => {
+    const tree = new Tree()
+    tree.insertNode(101)
+    tree.insertNode(33)
+    tree.insertNode(9)
+    tree.insertNode(39)
+    tree.insertNode(105)
+    tree.insertNode(104)
+    tree.insertNode(144)
+    return tree
+}
+
+tree2 = dataPoint2()
 tree2.printOrder(tree2.root, 3)
+// tree2.printOrder(tree2.root, 2)
+// tree2.printOrder(tree2.root, 3)
+
+// tree3 = dataPoint3()
+// tree3.printOrder(tree3.root, 2)
